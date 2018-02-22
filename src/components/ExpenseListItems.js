f5a70624956fb9cf566a722abba940317d8a4fc6
UJwal Arak <rootUJ@localhost.localdomain>
2018-02-22 19:02:45 +0530

    added timestamp to proper date and added currency to amount in  dashboard page

--- src/components/ExpenseListItems.js
+++ src/components/ExpenseListItems.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 const ExpenseListItem = ({ id, dispatch, description, amount, createdAt})=>(
     <div>
     <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-    <h4>{amount}-{createdAt}</h4>
-       
+    <h4>
+    {numeral(amount).format('$0,0.00')}
+    -
+    {moment(createdAt).format('Do MMMM YYYY')}</h4>  
     </div>
 );
 
